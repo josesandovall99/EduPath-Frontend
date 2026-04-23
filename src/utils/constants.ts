137df6d94a77243de665731b33e74387cda7adbd
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2026-04-23 11:49:46 -0500

    Agreego cambios chatbot

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@
 export const DEFAULT_API_BASE_URL = 'https://edupath-backend-xch1.onrender.com';
 
 //Par aq probremos en local
-//export const DEFAULT_API_BASE_URL = 'http://localhost:4000/';
+//export const DEFAULT_API_BASE_URL = 'http://localhost:4000';
 
 
 const rawApiBaseUrl = String((import.meta as any)?.env?.VITE_API_BASE_URL || '').trim();
