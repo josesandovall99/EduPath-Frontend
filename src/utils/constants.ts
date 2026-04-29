aad5c4598c20469787f77d83ce25ff9ecadda1d9
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2026-04-29 16:25:43 -0500

    Agrego URL del backend en el servidor de la U

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,9 @@
 //export const DEFAULT_API_BASE_URL = 'https://edupath-backend-xch1.onrender.com';
 
 //Par aq probremos en local
-export const DEFAULT_API_BASE_URL = 'http://localhost:4000';
+///PARA EL SERVIDOR DE LA UNIVERSIDAD: http://192.168.3.21:4000
+// PARA LOCALHOST: http://localhost:4000
+export const DEFAULT_API_BASE_URL = 'http://192.168.3.21:4000';
 
 
 const rawApiBaseUrl = String((import.meta as any)?.env?.VITE_API_BASE_URL || '').trim();
